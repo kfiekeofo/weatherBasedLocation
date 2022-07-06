18dbb398480ba5f7cf0b893da9853d4839a30325
김영덕 <112154@koreainvestment.com>
2022-07-06 14:31:57 +0900

    Initial Commit

---
+++ weatherBasedLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherBasedLocation
+//
+//  Created by kimyoungduck on 2022/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
